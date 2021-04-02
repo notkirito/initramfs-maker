220b7ee776dce83bc4813073856e5f122f2b7523
notkirito <73433481+notkirito@users.noreply.github.com>
2021-04-01 20:55:01 -0400

    I AM DONE WITH INIT YAY

--- mkinitramfs.sh
+++ mkinitramfs.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+# the actual script that makes the initramfs!
+
+# Licensed under the GPL-v3
+
+read_line() {
+    # The first positional argument is printf-ed, then
+    #+ this function reads a line and dumps it to stdout
+    #+ example usage: variable="$(read_line )"
+
+    # we actually want to keep the escape sequences
+    # shellcheck disable=SC2059
+    printf "$1"
+    cat /dev/stdin
+}
